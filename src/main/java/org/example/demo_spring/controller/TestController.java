6fa0af3023f93366e56651bf292a38a3e7043c90
Ashwin <ashwanibelani10@gmail.com>
2026-05-12 17:44:09 +0530

    Initial commit

---
+++ src/main/java/org/example/demo_spring/controller/TestController.java
@@ -0,0 +1,86 @@
+package org.example.demo_spring.controller;
+
+import org.example.demo_spring.model.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/*
+
+        STATUS CODE:
+        200  -  SUCCESS
+        400  -  CLIENT SIDE ERROR
+        500  -  SERVER SIDE ERROR
+
+
+
+ */
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String testGet()
+    {
+        return "hello, this is spring boot";
+    }
+
+    //testfetch
+    @GetMapping("/testpathvariable/{name}/{city}")
+    public String testPathVariable(@PathVariable String name, @PathVariable String city)
+    {
+        System.out.println("name "+name);
+        return "hello, this is spring boot, testFetch, name = "+name+", city "+city;
+    }
+
+    @GetMapping("/testparam")
+    public String testParam(@RequestParam String name, @RequestParam String city)
+    {
+        System.out.println("name "+name);
+        return "hello, this is spring boot, testFetch, name = "+name+", city "+city;
+    }
+
+    @GetMapping("/testparamsingle")
+    public String testParamSingle(@RequestParam String name)
+    {
+        System.out.println("name "+name);
+        return "hello, this is spring boot, testFetch, name = "+name;
+    }
+
+    @GetMapping("/testgetrequestbody")
+    @ResponseBody
+    public String testRequestBody(@RequestBody Student student)
+    {
+        System.out.println("student name "+student.getName());
+        return "hello, this is spring boot, testFetch, student name = "+student.getName();
+    }
+
+    @PostMapping("/testgetrequestbodypost")
+    public String testRequestBodyPost(@RequestBody Student student)
+    {
+        System.out.println("student name "+student.getName());
+        return "hello, this is spring boot, testFetch, student name = "+student.getName();
+    }
+
+    @GetMapping("/testreturnobject")
+    //@ResponseBody // compulsory with @Controller
+    public Student testReturnObject()
+    {
+        Student student = new Student();
+        student.setName("ashish");
+        return student;
+    }
+
+    @GetMapping("/testresponseentity")
+    public ResponseEntity<Student> testResponseEntity()
+    {
+        Student student = new Student();
+        student.setName("ashish");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(student);
+    }
+
+
+
+
+}
---
+++ src/main/java/org/example/demo_spring/model/Student.java
@@ -0,0 +1,44 @@
+package org.example.demo_spring.model;
+
+public class Student
+{
+    int id;
+
+    String name;
+
+    String city;
+
+    int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
